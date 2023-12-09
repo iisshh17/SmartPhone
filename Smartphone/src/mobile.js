79f25a115b2dad8b2027183b94e3b9af4fbcbe42
iisshh17 <144683616+iisshh17@users.noreply.github.com>
2023-12-09 14:40:07 +0530

    Add files via upload

---
+++ Smartphone/src/mobile.js
@@ -0,0 +1,87 @@
+// Progression 1 SmartPhone
+
+class SmartPhone {
+  constructor(ram, batteryPower, price){
+    this.ram = ram
+    this.batteryPower = batteryPower
+    this.price = price
+  }
+  displayFeatures=()=>{
+    return `${this.ram}\n${this.batteryPower}\n${this.price}`
+  }
+
+  remainingAmount(money){
+    this.price=this.price-money
+    if(this.price>0){
+      return this.price
+    }
+  }
+}
+
+// Progression 2 AndroidPhone
+
+class AndroidPhone extends SmartPhone {
+  constructor(modelName,ram, batteryPower, price){
+    super(ram,batteryPower,price)
+    this.modelName=modelName
+  }
+  displayFeatures=()=>{
+    return `${this.modelName}\n${this.ram}\n${this.batteryPower}\n${this.price}`
+  }
+  remainingAmount(money){
+    this.price=money-this.price
+    if(this.price<=0){
+      return `Customer has no more amount`
+    }
+    else{
+      return `Customer has remaining Rs.${this.price} after buying the android phone`
+    }
+  }
+}
+
+// Progression 3 IPhone
+
+class IPhone extends SmartPhone {
+  constructor(seriesName, ram, batteryPower, price){
+    super(ram, batteryPower, price)
+    this.seriesName = seriesName
+  }
+  displayFeatures=()=>{
+    return `${this.seriesName}\n${this.ram}\n${this.batteryPower}\n${this.price}`
+  }
+  remainingAmount(money){
+    this.price = money-this.price
+    if(this.price>0){
+      return `Customer has remaining Rs.${this.price} after buying an iphone`
+    }
+   else if(this.price<0){
+      return `Customer can't able to buy a phone due to insufficient amount`
+    }
+    else{
+      return `Customer has no more amount`
+    }
+  }
+}
+
+// Progression 4  Progression 1 MobileCampus
+
+class MobileCampus {
+  constructor(NoOfMobiles,NoOfAndroidMobiles,NoOfIPhoneMobiles){
+  this.NoOfMobiles = NoOfMobiles,
+  this.NoOfAndroidMobiles = NoOfAndroidMobiles,
+  this.NoOfIPhoneMobiles = NoOfIPhoneMobiles
+}
+
+changeMobileAvailabilityNumber(count,type){
+  if(type === 'android'){
+    this.NoOfMobiles = this.NoOfMobiles-count
+    this.NoOfAndroidMobiles = this.NoOfAndroidMobiles - count;
+    return `Available android mobiles are ${this.NoOfAndroidMobiles}`;
+
+}
+if(type === 'iphone'){
+    this.NoOfIPhoneMobiles = this.NoOfIPhoneMobiles - count;
+    return `Available iphone mobiles are ${this.NoOfIPhoneMobiles}`
+}
+}
+}
